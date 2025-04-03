2a90110b1ed27eefd45128a8171431df49616411
cyrilbarthelet <88724154+CyrilBarthelett@users.noreply.github.com>
2025-04-03 08:57:05 +0200

    Add files via upload

---
+++ Task 2/template_solution.py
@@ -0,0 +1,79 @@
+# This serves as a template which will guide you through the implementation of this task.  It is advised
+# to first read the whole template and get a sense of the overall structure of the code before trying to fill in any of the TODO gaps
+# First, we import necessary libraries:
+import numpy as np
+import pandas as pd
+
+def data_loading():
+    """
+    This function loads the training and test data, preprocesses it, removes the NaN values and interpolates the missing 
+    data using imputation
+
+    Parameters
+    ----------
+    Returns
+    ----------
+    X_train: matrix of floats, training input with features
+    y_train: array of floats, training output with labels
+    X_test: matrix of floats: dim = (100, ?), test input with features
+    """
+    # Load training data
+    train_df = pd.read_csv("train.csv")
+    
+    print("Training data:")
+    print("Shape:", train_df.shape)
+    print(train_df.head(2))
+    print('\n')
+    
+    # Load test data
+    test_df = pd.read_csv("test.csv")
+
+    print("Test data:")
+    print(test_df.shape)
+    print(test_df.head(2))
+
+    # Dummy initialization of the X_train, X_test and y_train
+    # TODO: Depending on how you deal with the non-numeric data, you may want to 
+    # modify/ignore the initialization of these variables   
+    X_train = np.zeros_like(train_df.drop(['price_CHF'],axis=1))
+    y_train = np.zeros_like(train_df['price_CHF'])
+    X_test = np.zeros_like(test_df)
+
+    # TODO: Perform data preprocessing, imputation and extract X_train, y_train and X_test
+
+    assert (X_train.shape[1] == X_test.shape[1]) and (X_train.shape[0] == y_train.shape[0]) and (X_test.shape[0] == 100), "Invalid data shape"
+    return X_train, y_train, X_test
+
+
+class Model(object):
+    def __init__(self):
+        super().__init__()
+        self._x_train = None
+        self._y_train = None
+
+    def train(self, X_train: np.ndarray, y_train: np.ndarray):
+        #TODO: Define the model and fit it using (X_train, y_train)
+        self._x_train = X_train
+        self._y_train = y_train
+
+    def predict(self, X_test: np.ndarray) -> np.ndarray:
+        y_pred=np.zeros(X_test.shape[0])
+        #TODO: Use the model to make predictions y_pred using test data X_test
+        assert y_pred.shape == (X_test.shape[0],), "Invalid data shape"
+        return y_pred
+
+# Main function. You don't have to change this
+if __name__ == "__main__":
+    # Data loading
+    X_train, y_train, X_test = data_loading()
+    model = Model()
+    # Use this function for training the model
+    model.train(X_train=X_train, y_train=y_train)
+    # Use this function for inferece
+    y_pred = model.predict(X_test)
+    # Save results in the required format
+    dt = pd.DataFrame(y_pred) 
+    dt.columns = ['price_CHF']
+    dt.to_csv('results.csv', index=False)
+    print("\nResults file successfully generated!")
+
